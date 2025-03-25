393a96fbf37d7e9671353a9605cfeb3042c3745c
Mohamed Nagy <bettermailmoe@gmail.com>
2025-03-24 21:15:47 -0400

    Test Repo Name Change

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const About: React.FC = () => {
   return (
     <div className="about p-4 text-primaryText">
-      <h2>About Page</h2>
+      <h2>About Pages</h2>
     </div>
   );
 };
