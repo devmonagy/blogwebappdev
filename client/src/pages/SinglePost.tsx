300deebe795247769c82ad88a3a97f23675adb87
Mohamed Nagy <bettermailmoe@gmail.com>
2025-04-16 16:15:46 -0400

    Single Post Skeleton Loader

--- client/src/pages/SinglePost.tsx
+++ client/src/pages/SinglePost.tsx
@@ -255,7 +255,20 @@ const SinglePost: React.FC = () => {
     );
   };
 
-  if (!post) return <div className="p-4">Loading...</div>;
+  if (!post) {
+    return (
+      <div className="container p-7 bg-background min-h-screen py-8 lg:max-w-screen-md">
+        <div className="mx-auto p-4 shadow rounded-lg bg-white animate-pulse space-y-4">
+          <div className="h-6 bg-gray-300 rounded w-3/4" />
+          <div className="h-4 bg-gray-300 rounded w-1/2" />
+          <div className="h-64 bg-gray-200 rounded" />
+          <div className="h-4 bg-gray-300 rounded w-full" />
+          <div className="h-4 bg-gray-300 rounded w-5/6" />
+          <div className="h-4 bg-gray-300 rounded w-2/3" />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="container p-7 bg-background min-h-screen py-8 lg:max-w-screen-md">
